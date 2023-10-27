3fcf70f625964151be0fe5bd5e9dcd941d8d5fc7
AksA1210 <95520592+AksA1210@users.noreply.github.com>
2023-10-27 06:38:16 +0530

    Update Policy.js

--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -1,28 +1,31 @@
 import React from "react";
-import Layout from "./../components/Layout/Layout";
+// import Layout from "./../components/Layout/Layout";
 
 const Policy = () => {
   return (
-    <Layout>
+    // <Layout>
       <div className="row contactus ">
         <div className="col-md-6 ">
-          <img
+          {/* <img
             src="/images/contactus.jpeg"
             alt="contactus"
             style={{ width: "100%" }}
-          />
+          /> */}
         </div>
-        <div className="col-md-4">
+        <div className="col-md-6">
+          <p>We know that you care how information about you is used and shared, and we appreciate your trust that we will do so carefully and sensibly. 
+
+</p>
+          <p></p>This Privacy Notice describes how Amazon Seller Services Private Limited and its affiliates including Amazon.com, Inc. (collectively "Amazon") collect and process your personal information through Amazon websites, devices, products, services, online marketplace and applications that reference this Privacy Notice (together "Amazon Services").
+          <p>You also agree and consent to us collecting, storing, processing, transferring, and sharing your personal information (including sensitive personal information) with third parties or service providers for the purposes set out in this Privacy Notice.</p>
+          <p>By using our services you agree to our use of your personal information (including sensitive personal information) in accordance with this Privacy Notice, as may be amended from time to time by us at our discretion. 
+</p>
+          {/* <p>add privacy policy</p>
           <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
+          <p>add privacy policy</p> */}
         </div>
       </div>
-    </Layout>
+    //{/* </Layout> */}
   );
 };
 
