9f88b237252393c841a6e31396d641cdf3245a72
AksA1210 <95520592+AksA1210@users.noreply.github.com>
2023-10-27 06:16:26 +0530

    Update About.js

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,30 +1,42 @@
 import React from "react";
-import Layout from "./../components/Layout/Layout";
+import "./About.css";
+import aboutImage from "../assets/stack_of_books.jpg";
 
 const About = () => {
   return (
-    <Layout>
-      <div className="row contactus ">
-        <div className="col-md-6 ">
-          <img
-            src="/images/about.jpeg"
-            alt="contactus"
-            style={{ width: "100%" }}
-          />
-        </div>
-        <div className="col-md-4">
-          <p className="text-justify mt-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            officiis obcaecati esse tempore unde ratione, eveniet mollitia,
-            perferendis eius temporibus dicta blanditiis doloremque explicabo
-            quasi sunt vero optio cum aperiam vel consectetur! Laborum enim
-            accusantium atque, excepturi sapiente amet! Tenetur ducimus aut
-            commodi illum quidem neque tempora nam.
+    <div className="about-container">
+      <div className="about-header">
+        <h1>About Our Platform</h1>
+        <p>
+          Our platform is dedicated to helping bookworms around
+          the world discover new and books. Whether you're a fantasy lover
+          or a romanticist, our platform has something for everyone.
+        </p>
+      </div>
+      <div className="about-content">
+        <div className="about-text">
+          <h2>Our Mission</h2>
+          <p>
+          Our mission is to provide a platform for users to purchase books online.Our website aims to make the process of buying books more convenient and accessible for users. 
+          Booktok offers a wide range of books, including new releases, bestsellers, and rare books. 
+          We also provide a platform for independent authors to sell their books.We want to provide a seamless user experience by offering features such as personalized recommendations, easy search options, and secure payment methods. 
           </p>
+          <h2>How It Works</h2>
+          <p>
+            Users can create a free account and start browsing their favorite
+            books. 
+            They can save their favorites, add to cart and pay for their required books.
+          </p>
+          
+        </div>
+        <div className="about-image">
+          <img src={aboutImage} alt="About Us" />
         </div>
       </div>
-    </Layout>
+    </div>
   );
 };
 
 export default About;
+
+
