7f906883280b27b7b07822fda18442dd52cf6658
AksA1210 <95520592+AksA1210@users.noreply.github.com>
2023-10-27 06:33:56 +0530

    Update HomePage.js

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Layout from "./../components/Layout/Layout";
+import HeroSection from './HeroSection';
+
 
 const HomePage = () => {
   return (
     <Layout>
-      <h1>Welcome to the store !!!</h1>
+      
+      <HeroSection />
     </Layout>
+
   );
 };
 
