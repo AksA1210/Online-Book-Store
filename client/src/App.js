9741a2b62f499ae1619f50fbe25592be2d02a54f
AksA1210 <95520592+AksA1210@users.noreply.github.com>
2023-11-10 10:15:07 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 
+import { useEffect } from 'react';
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
+import Contacts from "./pages/Contact";
 import Policy from "./pages/Policy";
 import Pagenotfound from "./pages/Pagenotfound";
 import Register from "./pages/Auth/Register";
