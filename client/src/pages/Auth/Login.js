0916b0e318899d0289f316c7d58f299945668224
AksA1210 <95520592+AksA1210@users.noreply.github.com>
2023-11-10 10:36:30 +0530

    Update Login.js

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -38,7 +38,7 @@ const Login = () => {
      
         if(credentials.email == "aksaaa@gmail.com" && credentials.password == "12345"){
             console.log("Login Successful !!!")
-            navigate('/main');
+            navigate('/book');
         }else{console.log("Login Failed");
 
         }
